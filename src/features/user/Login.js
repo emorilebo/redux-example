382b323acbe61400a4337a162130e17b4ccefa77
emorilebo <emorylebo@gmail.com>
2021-12-17 23:53:34 +0100

    Adjustments to the login

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { setUser, logout } from "./userSlice";
+import { useDispatch } from "react-redux";
 
 function Login() {
+  const dispatch = useDispatch();
+  const loginToApp = () => {
+    dispatch(setUser({ user: "Godfrey" }));
+  };
   return (
     <div>
-      <h2>I am the login component</h2>
+      <h2>I am the login component of the redux</h2>
+      <button onClick={loginToApp}>Log in here</button>
+      <button onClick={() => dispatch(logout())}>LOGOUT</button>
     </div>
   );
 }
